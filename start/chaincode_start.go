d86147a30e50c5348d8b30f8dd07034da0ada539
Wei Hu <venushuwwei@gmail.com>
2017-03-06 19:52:35 +0800

    test read

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -97,16 +97,21 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	return nil, errors.New("Received unknown function query: " + function)
 }
 
+// read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
 	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
-	name := args[0]
-	valAsbytes, err := stub.GetState(name)
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + name + "\"}"
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
+
 	return valAsbytes, nil
 }
